85050972b33ce59d8dd96142bcf98fc774ebbf92
Zbigniew Malcherczyk <zmalcherczyk@gmail.com>
2019-11-11 14:25:06 +0100

    Create codecov test (#3)

---
+++ src/Domain/Repository.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Domain;
+
+final class Repository
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
+}
